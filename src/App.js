bb71599447549406f0210588a6447117508c32ef
Mehefujur Mubin <mehefujurmubin@gmail.com>
2025-06-02 23:12:20 +0600

    app.js is updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import Portfolio from "./Portfolio";
 
 function App() {
-  return <Portfolio />;
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Mehefujur Mubin</h1>
+      <p className="text-lg text-gray-600">Machine Learning Engineer | Data Scientist | Researcher</p>
+      <p className="mt-4 text-blue-600">
+        <a href="mailto:mehefujurmubin@gmail.com">mehefujurmubin@gmail.com</a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
